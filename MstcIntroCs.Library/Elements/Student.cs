858cc5d3fac3b0fa6ca782e7f46a30f480d5a094
fferegrino <antonio.feregrino@gmail.com>
2014-10-09 22:48:17 -0500

    Pusheando código, pues.

---
+++ MstcIntroCs.Library/Elements/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MstcIntroCs.Library.Elements
+{
+    public class Student
+    {
+        #region Propiedades ejemplo
+        // Propiedad autoimplementada:
+        public string Name { get; set; }
+
+        // Propiedad:
+        private string _name;
+        //// Propiedad completa:
+        //public string Name
+        //{
+        //    get { return _name; }
+        //    set { _name = value; }
+        //}
+
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        // Propiedad nullable
+        public DateTime? GraduationDate { get; set; }
+        // Equivalent: public Nullable<DateTime> { get; set; }
+
+
+        #endregion
+
+        #region Propiedades de Student
+        public int Level { get; set; }
+        
+        /// <summary>
+        /// Grade Point Average
+        /// </summary>
+        public decimal Gpa { get; set; }
+
+        public List<Course> Courses { get; set; }
+
+        public string Nickname { get; set; }
+        #endregion
+
+        #region Métodos ejemplo
+        /// <summary>
+        /// Método sobreescrito:
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name + "(" + (Nickname ?? "no nickname") + ")";
+        }
+        #endregion
+    }
+}
---
+++ MstcIntroCs.Library/Extensions/StudentExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MstcIntroCs.Library.Elements;
+
+namespace MstcIntroCs.Library.Extensions
+{
+    public static class StudentExtensions
+    {
+        /// <summary>
+        /// Adds a course to a student
+        /// </summary>
+        /// <param name="student"></param>
+        /// <param name="course"></param>
+        public static void AddCourse(this Student student, Course course)
+        {
+            if(student.Courses == null)
+            {
+                student.Courses = new List<Course>();
+            }
+            student.Courses.Add(course);
+        }
+    }
+}
