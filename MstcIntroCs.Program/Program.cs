540431d8867fc8f6802e479ea0f9342c0e192d5b
fferegrino <antonio.feregrino@gmail.com>
2014-12-02 06:07:31 -0600

    Arrays bla bla bla

--- MstcIntroCs.Program/Program.cs
+++ MstcIntroCs.Program/Program.cs
@@ -51,6 +51,48 @@ namespace MstcIntroCs.Program
 
             #endregion
 
+            #region Arreglos
+
+            //char[] vocales = new char[5];
+            //vocales[4] = 'u';
+            //char[] vocales = null; new char[] { 'a', 'e', 'i', 'o', 'u' };
+            //char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+            //char[,] gato = new char[5, 3];
+            //char[,] gato = // new char[,]
+            //{
+            //    {'o', 'x', 'o'},
+            //    {'o', 'x', 'o'},
+            //    {'o', 'x', 'o'}
+            //};
+
+            //char[][] gato = new char[3][];
+            //{
+            //    new char [] {'o', 'x'},
+            //    new char [] {'o', 'x', 'o'},
+            //    new char [] {'o'}
+            //};
+            #endregion
+
+            #region Booleans
+
+            //if (Funcion1(false) && Funcion2(true))
+            //{
+            //    Console.WriteLine("Hola!");
+            //}
+
+            //Console.WriteLine(new String('-', 10));
+
+            //if (false && true)
+            //{
+
+            //}
+
+            //if (Funcion1(false) & Funcion2(true))
+            //{
+            //    Console.WriteLine("Hola!");
+            //}
+            #endregion
+
             #region Enums
             //Teacher hardTeacher = new Teacher("Hard teacher");
             //hardTeacher.TeacherDifficulty = Difficulty.Easy;
@@ -68,17 +110,17 @@ namespace MstcIntroCs.Program
             #endregion
 
             #region Generics
-            Bus<Book> busOfBooks = new Bus<Book>();
-            Bus<Teacher> busOfTeachers = new Bus<Teacher>();
+            //Bus<Book> busOfBooks = new Bus<Book>();
+            //Bus<Teacher> busOfTeachers = new Bus<Teacher>();
             #endregion
 
             #region Linq
 
-            Truck<Book> bookTruck = Truck<Book>.CreateSampleTruck();
-            Console.WriteLine(String.Format("The book truck has {0} items", bookTruck.Count));
-            List<Book> openedBooks = bookTruck.GetAllOpenedStuff();
-            Console.WriteLine(String.Format("The book truck has {0} opened items", openedBooks.Count));
-            
+            //Truck<Book> bookTruck = Truck<Book>.CreateSampleTruck();
+            //Console.WriteLine(String.Format("The book truck has {0} items", bookTruck.Count));
+            //List<Book> openedBooks = bookTruck.GetAllOpenedStuff();
+            //Console.WriteLine(String.Format("The book truck has {0} opened items", openedBooks.Count));
+
 
             #endregion
 
@@ -90,5 +132,19 @@ namespace MstcIntroCs.Program
         {
             thing.Open();
         }
+
+
+        public static bool Funcion1(bool valor)
+        {
+            Console.WriteLine("Funcion uno");
+            return valor;
+        }
+
+        public static bool Funcion2(bool valor)
+        {
+            Console.WriteLine("Funcion dos");
+            return valor;
+        }
+
     }
 }
