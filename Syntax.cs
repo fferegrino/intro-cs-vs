4128993409156d2d69248ae3a10ac2b97135a4de
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-04-02 12:29:56 -0600

    Hello world! (again)

---
+++ Syntax.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Syntax
+{
+    // Entry point
+    static void Main()
+    {
+		int fVersion = 10 * (1 + 1); 
+		Console.WriteLine("Hello world! (I'm in Fedora " + fVersion + ").");
+    }
+}
