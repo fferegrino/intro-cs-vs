0cc4f30e23251d56cd6ff8f7781f766bb6234d26
Antonio Feregrino <antonio.feregrino@gmail.com>
2015-04-03 12:23:44 -0600

    Two more classes

---
+++ ValueAndReferenceTypes.cs
@@ -0,0 +1,46 @@
+using System;
+
+class ValueAndReferenceTypes
+{
+
+    static void Main(string[] args)
+    {
+        var timeReference1 = new TimeReference();
+        TimeReference timeReference2 = timeReference1;
+
+        // Modify only one instance
+        timeReference1.Minutes = 15;
+        timeReference1.Hours = 12;
+
+        Console.WriteLine("Reference types:");
+        Console.WriteLine("timeReference1:\tHours: " + timeReference1.Hours + "\tMinutes: " + timeReference1.Minutes);
+        Console.WriteLine("timeReference2:\tHours: " + timeReference2.Hours + "\tMinutes: " + timeReference2.Minutes);
+
+        var timeValue1 = new TimeValue();
+        TimeValue timeValue2 = timeValue1;
+
+        // Modify only one instance
+        timeValue1.Minutes = 15;
+        timeValue1.Hours = 12;
+
+        Console.WriteLine("Value types:");
+        Console.WriteLine("timeValue1:\tHours: " + timeValue1.Hours + "\tMinutes: " + timeValue1.Minutes);
+        Console.WriteLine("timeValue2:\tHours: " + timeValue2.Hours + "\tMinutes: " + timeValue2.Minutes);
+
+
+        // Uncomment if running in Visual Studio:
+        Console.ReadKey();
+    }
+}
+
+struct TimeValue
+{
+    public int Hours;
+    public int Minutes;
+}
+
+class TimeReference
+{
+    public int Hours;
+    public int Minutes;
+}
---
+++ VarsAndDataTypes.cs
@@ -0,0 +1,32 @@
+using System;
+
+class VarsAndDataTypes
+{
+
+	static int lolIwork;
+	static char lolIwork2;
+
+	static void Main(string[] args)
+	{
+		int lolIdontWork;
+		var IknowWhatIAm = 'a';
+		var IknowToo = 1000;
+		//// This must be initialized to something
+		//var NoOneKnows;
+
+		Console.WriteLine(lolIwork);
+		Console.WriteLine(lolIwork2);
+
+		//// This won't run:
+		//Console.WriteLine(lolIdontWork);
+
+		lolIdontWork = 30;
+		// This now works:
+		Console.WriteLine(lolIdontWork);
+		
+
+		//// Uncomment if running in Visual Studio:
+		//Console.ReadKey();
+	}
+}
+
